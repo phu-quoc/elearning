6484ed83f0e095843757f39fdaf47a9a0a610ddc
Le Phu Quoc <tygovip119@gmail.com>
2022-05-19 22:33:24 +0700

    Seeder

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('/courses', CourseController::class);
