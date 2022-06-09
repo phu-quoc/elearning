2203c1caef323f3a25c47ad04b3d8b9cfc294fd4
Le Phu Quoc <tygovip119@gmail.com>
2022-06-09 12:14:11 +0700

    Notification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Notification\FcmService;
+use App\Services\Notification\NotificationService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(NotificationService::class, function(){
+            return new FcmService();
+        });
     }
 
     /**
